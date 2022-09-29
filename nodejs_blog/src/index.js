17ba541102140ccd7b1ea595862c8508dd085e6c
phamphuc2005 <minhpham2001bk@gmail.com>
2022-09-29 21:35:58 +0700

    routes & controller

--- nodejs_blog/src/index.js
+++ nodejs_blog/src/index.js
@@ -5,6 +5,8 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3002;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({
@@ -20,21 +22,7 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
-
-app.get('/search', (req, res) => {
-  res.render('search');
-});
-
-app.post('/search', (req, res) => {
-  res.send('');
-});
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
