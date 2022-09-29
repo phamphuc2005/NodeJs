8f0b27f8bb5c9fea14781bd3fd20de2bc9efe7c8
phamphuc2005 <minhpham2001bk@gmail.com>
2022-09-29 20:26:36 +0700

    Form default behavior

--- nodejs_blog/src/index.js
+++ nodejs_blog/src/index.js
@@ -17,11 +17,15 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
   res.render('home');
-})
+});
 
 app.get('/news', (req, res) => {
   res.render('news');
-})
+});
+
+app.get('/search', (req, res) => {
+  res.render('search');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
