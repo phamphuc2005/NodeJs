93c4e259ae8498500f7517918c4297ce4608adff
phamphuc2005 <minhpham2001bk@gmail.com>
2022-09-30 23:34:07 +0700

    delete course after update course

--- nodejs_blog/src/config/db/index.js
+++ nodejs_blog/src/config/db/index.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/education_dev');
+        await mongoose.connect('mongodb://localhost:27017/education_dev', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log('Connect ok!');
     } catch (error) {
         console.log('Connect fail!');
