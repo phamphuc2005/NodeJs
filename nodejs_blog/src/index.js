c8a3a8de02290d5c363bbcd7d1088701d308b1cb
phamphuc2005 <minhpham2001bk@gmail.com>
2022-09-30 21:55:42 +0700

    delete course

--- nodejs_blog/src/index.js
+++ nodejs_blog/src/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const methodOverride = require('method-override');
 const handlebars = require('express-handlebars');
 const app = express();
 const port = 3002;
@@ -19,12 +20,17 @@ app.use(
 );
 app.use(express.json());
 
+app.use(methodOverride('_method'));
+
 app.use(morgan('combined'));
 
 app.engine(
     'hbs',
     handlebars.engine({
         extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        },
     }),
 );
 app.set('view engine', 'hbs');
