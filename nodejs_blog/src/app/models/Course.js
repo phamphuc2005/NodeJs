ed4f6cbb949a8a4245c4d4c1ab08677436c6b055
phamphuc2005 <minhpham2001bk@gmail.com>
2022-09-30 17:26:59 +0700

    course detail page

--- nodejs_blog/src/app/models/Course.js
+++ nodejs_blog/src/app/models/Course.js
@@ -7,6 +7,7 @@ const Course = new Schema({
     image: { type: String, maxLength: 255 },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
+    slug: {type: String},
   });
 
   module.exports = mongoose.model('Course', Course);
