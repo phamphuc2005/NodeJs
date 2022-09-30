037c230184d1061db3ecac1bac7c9b2d424a1bee
phamphuc2005 <minhpham2001bk@gmail.com>
2022-09-30 11:35:15 +0700

    read from db

--- nodejs_blog/src/app/controllers/SiteController.js
+++ nodejs_blog/src/app/controllers/SiteController.js
@@ -1,14 +1,22 @@
 const Course = require('../models/Course');
+const { mutipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
-    index(req, res) {
-        Course.find({}, function(err, courses) {
-            if (!err) {
-                res.json(courses);
-            } else {
-                res.status(400).json({ error: 'ERROR!!!' });
-            }
-        });
+    index(req, res, next) {
+        // Course.find({}, function(err, courses) {
+        //     if (!err) {
+        //         res.json(courses);
+        //     } else {
+        //         next(err);
+        //     }
+        // });
+
+        Course.find({})
+            .then(courses => {
+                res.render('home', {courses: mutipleMongooseToObject(courses)})
+            })
+            .catch(next);
+
         // res.render('home');
     }
 
