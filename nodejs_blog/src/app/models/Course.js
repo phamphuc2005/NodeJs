3d5c52fc7ece15a0000a0683951ef399226e3e2f
phamphuc2005 <minhpham2001bk@gmail.com>
2022-09-30 19:39:58 +0700

    create new course

--- nodejs_blog/src/app/models/Course.js
+++ nodejs_blog/src/app/models/Course.js
@@ -1,13 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const slug = require('mongoose-slug-generator');
+
+mongoose.plugin(slug);
 
 const Course = new Schema({
-    name: {type: String, maxLength: 255},
+  name: { type: String, required: true },
     description: { type: String, maxLength: 600 },
-    image: { type: String, maxLength: 255 },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
-    slug: {type: String},
-  });
+    image: { type: String},
+    slug: { type: String, slug: 'name', unique: true },
+    videoId: { type: String, required: true },
+    level: { type: String },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-  module.exports = mongoose.model('Course', Course);
+module.exports = mongoose.model('Course', Course);
