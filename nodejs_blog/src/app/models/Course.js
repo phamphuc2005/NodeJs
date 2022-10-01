297343c5f220911c5a55d8dafdf3018d92e75916
phamphuc2005 <minhpham2001bk@gmail.com>
2022-10-01 16:14:33 +0700

    soft delete

--- nodejs_blog/src/app/models/Course.js
+++ nodejs_blog/src/app/models/Course.js
@@ -1,20 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const slug = require('mongoose-slug-generator');
-
-mongoose.plugin(slug);
+const mongooseDelete = require('mongoose-delete');
 
 const Course = new Schema({
   name: { type: String, required: true },
-    description: { type: String, maxLength: 600 },
-    image: { type: String},
-    slug: { type: String, slug: 'name', unique: true },
-    videoId: { type: String, required: true },
-    level: { type: String },
-  },
-  {
-    timestamps: true,
-  },
+  description: { type: String, maxLength: 600 },
+  image: { type: String},
+  slug: { type: String, slug: 'name', unique: true },
+  videoId: { type: String, required: true },
+  level: { type: String },
+  deletedAt: {},
+},
+{
+  timestamps: true,
+},
 );
+
+// Add plugins
+mongoose.plugin(slug);
+Course.plugin(mongooseDelete, {
+    deletedAt: true,
+    overrideMethods: 'all',
+});
 
 module.exports = mongoose.model('Course', Course);
